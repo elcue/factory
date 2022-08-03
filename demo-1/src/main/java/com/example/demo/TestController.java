ecc4abdf255fb0cf25455c5ea9d88013168f927b
user <user@host.docker.internal>
2022-08-03 16:32:02 +0900

    hello docker!

--- demo-1/src/main/java/com/example/demo/TestController.java
+++ demo-1/src/main/java/com/example/demo/TestController.java
@@ -1,8 +1,15 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+	
+	@RequestMapping("/")
+	public String test() {
+		
+		return "hello docker";
+	}
 
 }
